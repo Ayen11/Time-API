4e234a34fbb8d901f84ad778e8fff5bf50369737
Ayen <87607693+Ayen11@users.noreply.github.com>
2023-07-15 16:27:09 +0300

    finished setting up the HTTP request

--- APITime/Source/APITime/APITimeGameModeBase.cpp
+++ APITime/Source/APITime/APITimeGameModeBase.cpp
@@ -38,10 +38,20 @@ void AAPITimeGameModeBase::SendHTTPGet()
 void AAPITimeGameModeBase::OnGetTimeResponse(FHttpRequestPtr Request, FHttpResponsePtr Response,
 	bool bConnectedSuccesfully)
 {
+	// Creating the json object
 	TSharedPtr<FJsonObject> JsonObject;
+	// Checking if we are successful in our request (200 means the opposite of 404 basically)
 	if (Response->GetResponseCode() == 200)
 	{
+		// Store the response in our response body
 		const FString ResponseBody = Response->GetContentAsString();
+		// Creating a reader for the response body
 		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(ResponseBody);
+		
+		// Check if we are successful in reading the json 
+		if (FJsonSerializer::Deserialize(Reader, JsonObject))
+		{
+			// this is where we put the information on what to do with the data
+		}
 	}
 }
